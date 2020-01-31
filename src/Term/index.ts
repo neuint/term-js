663971c73f67fd5c3c923a20d2e3f727885993c9
Pavlo Gusar <pgusar@yahoo.com>
2020-01-31 23:04:43 +0200

    [#1] adding header

--- src/Term/index.ts
+++ src/Term/index.ts
@@ -6,6 +6,7 @@ import './theme.scss';
 
 class Term implements ITerm {
   private readonly container: Element;
+  private header: string = '';
 
   private readonly containers: {
     [key: string]: Element;
@@ -32,12 +33,35 @@ class Term implements ITerm {
     throw new Error('No implementation');
   }
 
+  public setHeader(text: string) {
+    const { main, header, headerText } = this.containers;
+    this.header = text;
+    if (text) {
+      headerText.innerHTML = text;
+      main.appendChild(header);
+    } else {
+      main.removeChild(header);
+    }
+  }
+
   protected addElements() {
     const { container, containers } = this;
     const main = document.createElement('div');
     main.className = css.term;
     containers.main = main;
+    this.addHeader();
     container.appendChild(main);
+  }
+
+  protected addHeader() {
+    const { containers } = this;
+    const header = document.createElement('div');
+    const headerText = document.createElement('span');
+    header.className = css.header;
+    headerText.className = css.headerText;
+    containers.header = header;
+    containers.headerText = headerText;
+    header.appendChild(headerText);
   }
 }
 
