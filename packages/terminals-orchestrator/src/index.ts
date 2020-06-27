06738b349641a8242ef3e314088c1521b01bdeac
Pavlo Gusar <p.gusar@yahoo.com>
2020-06-27 17:49:09 +0300

    [#68] clean up terminals-orchestrator

--- packages/terminals-orchestrator/src/index.ts
+++ packages/terminals-orchestrator/src/index.ts
@@ -1,2 +1,3 @@
 export { default as TerminalsOrchestrator } from '@TerminalsOrchestrator/index';
 export { default as ITerminalsOrchestrator } from '@TerminalsOrchestrator/ITerminalsOrchestrator';
+export { OptionsType, TabType, TermParamsType } from '@TerminalsOrchestrator/types';
