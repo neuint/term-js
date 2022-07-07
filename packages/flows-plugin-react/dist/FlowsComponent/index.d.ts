cab9d70d81597fbde50e780acf1245af2048bce2
Pavlo Gusar <p.gusar@yahoo.com>
2022-07-08 00:31:11 +0200

    #137 adding flows component

---
+++ packages/flows-plugin-react/dist/FlowsComponent/index.d.ts
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import { FlowsType } from '@neuint/flows-plugin';
+import { ITerm } from '@neuint/term-js';
+export type { FlowsType } from '@neuint/flows-plugin';
+declare type PropsType = {
+    term?: ITerm;
+    flows?: FlowsType;
+};
+declare const FlowsComponent: FC<PropsType>;
+export default FlowsComponent;
