a3a23a7f58e5658d2027fb14c90ff4b83f7e0a6a
Pavlo Gusar <p.gusar@yahoo.com>
2022-07-12 23:28:40 +0200

    build term-js-react

---
+++ packages/term-js-react/dist/packages/term-js-react/src/TermComponent/index.d.ts
@@ -0,0 +1,23 @@
+import React, { FC } from 'react';
+import { ValueType } from '@neuint/term-js';
+import { WriteType } from '@general/types/write';
+import '@neuint/term-js/dist/index.css';
+declare type HandlersType = {
+    onSubmit?: (line: string, lines: string[]) => void;
+    onChange?: (e: InputEvent) => void;
+};
+export type { WriteType, FullWriteType, WriteItemType } from '@general/types/write';
+declare type PropsType = {
+    className?: string;
+    header?: string;
+    label?: string;
+    delimiter?: string;
+    secret?: boolean;
+    initLines?: ValueType[];
+    initValue?: ValueType;
+    write?: WriteType;
+    onWritten?: () => void;
+    children?: React.ReactNode;
+} & HandlersType;
+declare const TermComponent: FC<PropsType>;
+export default TermComponent;
