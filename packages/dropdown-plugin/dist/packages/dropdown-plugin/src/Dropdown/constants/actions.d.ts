87bc98747331d6f880ef85677b380ffba6d4c53a
Pavlo Gusar <p.gusar@yahoo.com>
2022-07-12 23:27:55 +0200

    build dropdown-plugin

---
+++ packages/dropdown-plugin/dist/packages/dropdown-plugin/src/Dropdown/constants/actions.d.ts
@@ -0,0 +1,4 @@
+export declare const NEXT_ACTION = "dropdown-plugin-next";
+export declare const DOWN_ACTION = "dropdown-plugin-down";
+export declare const UP_ACTION = "dropdown-plugin-up";
+export declare const SUBMIT_ACTION = "dropdown-plugin-submit";
---
+++ packages/dropdown-plugin/dist/packages/dropdown-plugin/src/Dropdown/index.d.ts
@@ -0,0 +1,48 @@
+import { Plugin, ITermInfo, IKeyboardShortcutsManager, IPluginManager } from '@neuint/term-js';
+import '@neuint/context-menu-plugin/dist/index.css';
+import IDropdown from './IDropdown';
+export { default as IDropdown } from './IDropdown';
+declare class Dropdown extends Plugin implements IDropdown {
+    readonly name: string;
+    isActionsLock: boolean;
+    private itemsList;
+    get items(): string[];
+    set items(val: string[]);
+    private highlightField;
+    get highlight(): string;
+    set highlight(val: string);
+    private list?;
+    private contextMenuPlugin?;
+    private unlockCallback?;
+    private isActive;
+    private onSelect?;
+    private onClose?;
+    private readonly container;
+    private append?;
+    constructor(pluginManager: IPluginManager);
+    setTermInfo(termInfo: ITermInfo, keyboardShortcutsManager: IKeyboardShortcutsManager): void;
+    updateTermInfo(termInfo: ITermInfo): void;
+    clear(): void;
+    destroy(): void;
+    show(items?: string[], params?: {
+        className?: string;
+        append?: string | HTMLElement;
+        onSelect?: (text: string, index: number) => void;
+        onClose?: () => void;
+    }): void;
+    hide(): void;
+    private unregisterShortcut;
+    private registerShortcut;
+    private setContextMenuPlugin;
+    private onNext;
+    private onDown;
+    private onUp;
+    private onSubmit;
+    private renderList;
+    private renderAppend;
+    private clearAppend;
+    private selectHandler;
+    private hideContextMenuHandler;
+    private hideList;
+}
+export default Dropdown;
