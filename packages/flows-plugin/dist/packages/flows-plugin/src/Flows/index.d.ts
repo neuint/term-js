81004da8fcb8bd646387e240ae17d53a951951b8
Pavlo Gusar <p.gusar@yahoo.com>
2022-07-12 23:28:30 +0200

    build flows-plugin

---
+++ packages/flows-plugin/dist/packages/flows-plugin/src/Flows/index.d.ts
@@ -0,0 +1,21 @@
+import { Plugin, ITermInfo, IKeyboardShortcutsManager } from '@neuint/term-js';
+import IFlows from './IFlows';
+import { FlowsType } from './types';
+export type { FlowsType, FlowType, StepResultType } from './types';
+export type { default as IFlows } from './IFlows';
+declare class Flows extends Plugin implements IFlows {
+    private flowsField;
+    get flows(): FlowsType;
+    set flows(flows: FlowsType);
+    private branch?;
+    private step;
+    private branchData;
+    private isWaiting;
+    clear: (...args: any[]) => void;
+    setTermInfo(termInfo: ITermInfo, keyboardShortcutsManager: IKeyboardShortcutsManager): void;
+    private runAutoStartBranch;
+    private onSubmit;
+    private onStepResult;
+    private showStep;
+}
+export default Flows;
