222b76977437b893e655730af52678aa04d9f93a
Pavlo Gusar <p.gusar@yahoo.com>
2022-07-12 13:53:54 +0200

    build flows-plugin

--- packages/flows-plugin/dist/Flows/index.d.ts
+++ packages/flows-plugin/dist/Flows/index.d.ts
@@ -4,13 +4,16 @@ import { FlowsType } from './types';
 export type { FlowsType, FlowType, StepResultType } from './types';
 export type { default as IFlows } from './IFlows';
 declare class Flows extends Plugin implements IFlows {
-    flows: FlowsType;
+    private flowsField;
+    get flows(): FlowsType;
+    set flows(flows: FlowsType);
     private branch?;
     private step;
     private branchData;
     private isWaiting;
     clear: (...args: any[]) => void;
     setTermInfo(termInfo: ITermInfo, keyboardShortcutsManager: IKeyboardShortcutsManager): void;
+    private runAutoStartBranch;
     private onSubmit;
     private onStepResult;
     private showStep;
