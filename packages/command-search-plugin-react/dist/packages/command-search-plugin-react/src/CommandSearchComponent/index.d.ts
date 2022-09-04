eefa76aac370d5bca9765603d809ab93931ba131
Pavlo Gusar <p.gusar@yahoo.com>
2022-09-04 20:35:00 +0200

    build command-search-plugin-react

---
+++ packages/command-search-plugin-react/dist/packages/command-search-plugin-react/src/CommandSearchComponent/index.d.ts
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import { ITerm } from '@neuint/term-js';
+import '@neuint/command-search-plugin/dist/index.css';
+declare type PropsType = {
+    commands: string[];
+    autoOpen?: boolean;
+    term?: ITerm;
+};
+declare const CommandSearchComponent: FC<PropsType>;
+export default CommandSearchComponent;
